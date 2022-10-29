74ceda2ebb6fc16493c3302d9078e239e6e8a697
wendy barrios <wbarrios45@yahoo.com>
2022-10-29 12:27:51 -0700

    Method: funfact edited.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const characters = {
         'birthday': 'May 27, 1965',
         'species': 'Human',
         'height': '155 cm',
-        'fun fact':  'Mario has appeared in over 200 video games. '
+        'fun-fact':  'Mario has appeared in over 200 video games. '
 
     },
     'luigi': {
@@ -19,28 +19,28 @@ const characters = {
         'birthday': 'May 27, 1965',
         'species': 'Human',
         'height': '160 cm',
-        'fun fact':  "Luigi is the only one in the entire Mario franchise to have seen what's under a living Shy Guy's mask."
+        'fun-fact':  "Luigi is the only one in the entire Mario franchise to have seen what's under a living Shy Guy's mask."
     },
     'princess peach': {
         'age': 25,
         'birthday': 'January 8, 1965',
         'species': 'Human',
         'height': '160 cm',
-        'fun fact':  'Princess Peach has been featured in Forbes Magazine.'
+        'fun-fact':  'Princess Peach has been featured in Forbes Magazine.'
     },
     'princess daisy': {
         'age': 25,
         'birthday': 'April 21, 1985',
         'species': 'Human',
         'height': '159 cm',
-        'fun fact':  'Princess Daisy has her very own cruise liner, the Daisy Cruiser. '
+        'fun-fact':  'Princess Daisy has her very own cruise liner, the Daisy Cruiser. '
     },
     'yoshi': {
         'age': 25,
         'birthday': 'July 15, 1953',
         'species': 'T. Yoshisaur Munchakoopas',
         'height': '140 cm',
-        'fun fact':  'In the Super Mario World Japanese version, Yoshi can eat dolphins. '
+        'fun-fact':  'In the Super Mario World Japanese version, Yoshi can eat dolphins. '
 
     },
     'bowser': {
@@ -48,7 +48,7 @@ const characters = {
         'birthday': 'July 15, 1953',
         'species': 'T. Yoshisaur Munchakoopas',
         'height': '140 cm',
-        'fun fact':  'Aside from appearing in countless Nintendo titles, Bowser can also be seen in Wreck-It Ralph.'
+        'fun-fact':  'Aside from appearing in countless Nintendo titles, Bowser can also be seen in Wreck-It Ralph.'
 
     },
     'unknown': {
