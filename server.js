808268e001239f6565f666536bb53fd0da35aac2
wendy barrios <wbarrios45@yahoo.com>
2022-10-30 12:22:31 -0700

    Character method edited.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const characters = {
         'birthday': 'May 27, 1965',
         'species': 'Human',
         'height': '155 cm',
-        'funfact':  'Mario has appeared in over 200 video games. '
+        'funFact':  'Mario has appeared in over 200 video games. '
 
     },
     'luigi': {
@@ -19,7 +19,7 @@ const characters = {
         'birthday': 'May 27, 1965',
         'species': 'Human',
         'height': '160 cm',
-        'funfact':  "Luigi is the only one in the entire Mario franchise to have seen what's under a living Shy Guy's mask."
+        'funFact':  "Luigi is the only one in the entire Mario franchise to have seen what's under a living Shy Guy's mask."
     },
     'princess peach': {
         'age': 25,
@@ -33,14 +33,14 @@ const characters = {
         'birthday': 'April 21, 1985',
         'species': 'Human',
         'height': '159 cm',
-        'funfact':  'Princess Daisy has her very own cruise liner, the Daisy Cruiser. '
+        'funFact':  'Princess Daisy has her very own cruise liner, the Daisy Cruiser. '
     },
     'yoshi': {
         'age': 25,
         'birthday': 'July 15, 1953',
         'species': 'T. Yoshisaur Munchakoopas',
         'height': '140 cm',
-        'funfact':  'In the Super Mario World Japanese version, Yoshi can eat dolphins. '
+        'funFact':  'In the Super Mario World Japanese version, Yoshi can eat dolphins. '
 
     },
     'bowser': {
@@ -48,14 +48,15 @@ const characters = {
         'birthday': 'July 15, 1953',
         'species': 'T. Yoshisaur Munchakoopas',
         'height': '140 cm',
-        'funfact':  'Aside from appearing in countless Nintendo titles, Bowser can also be seen in Wreck-It Ralph.'
+        'funFact':  'Aside from appearing in countless Nintendo titles, Bowser can also be seen in Wreck-It Ralph.'
 
     },
     'unknown': {
         'age': 0,
         'birthday': 'unknown',
         'species': 'unknown',
-        'height': 'unknown'
+        'height': 'unknown',
+        'funFact': 'unknown'
 
     }
 
